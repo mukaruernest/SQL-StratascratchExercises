ef327d3422b4d8f5294b44bf5eb80d924a21ae38
Ernest Mukaru <mukaruernesto@gmail.com>
2021-07-03 19:32:50 +0300

    Number Of Units Per Nationality

---
+++ Number Of Units Per Nationality/NumberOfUnitsPerNationality.sql
@@ -0,0 +1,6 @@
+SELECT hs.nationality, count(un.unit_id) AS NumberOfAparts
+FROM airbnb_hosts hs
+    INNER JOIN airbnb_units un ON hs.host_id = un.host_id
+WHERE hs.age < 30 AND un.unit_type = 'Apartment'
+GROUP BY nationality
+ORDER BY NumberOfAparts DESC
