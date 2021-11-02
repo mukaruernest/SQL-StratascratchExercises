d37f328960bcc419ab76e7a5ab0389683596df82
mukaruernest <mukaruernesto@gmail.com>
2021-11-02 17:08:06 +0300

    fix

--- Review of Categories(UNNEST)/reviews.sql
+++ Review of Categories(UNNEST)/reviews.sql
@@ -1,9 +1,9 @@
-WITH cats AS
+WITH categories AS
   (SELECT unnest(string_to_array(categories, ';')) AS category,
           review_count
    FROM yelp_business)
 SELECT category,
        sum(review_count) as review_cnt
-FROM cats
+FROM categories
 GROUP BY category
 ORDER BY review_cnt DESC
