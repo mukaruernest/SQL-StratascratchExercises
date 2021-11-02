cff3ef256cae2fcfd18c66c5f4abd1544bece73a
mukaruernest <mukaruernesto@gmail.com>
2021-11-02 17:03:44 +0300

    reviews

---
+++ Review of Categories(UNNEST)/reviews.sql
@@ -0,0 +1,9 @@
+WITH cats AS
+  (SELECT unnest(string_to_array(categories, ';')) AS category,
+          review_count
+   FROM yelp_business)
+SELECT category,
+       sum(review_count) as review_cnt
+FROM cats
+GROUP BY category
+ORDER BY review_cnt DESC
